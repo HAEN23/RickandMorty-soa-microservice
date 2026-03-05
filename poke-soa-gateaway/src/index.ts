b2934ccf4ecf370dccd0f4c49ea1af344e83cc91
243691Heber <243691@ids.upchiapas.edu.mx>
2026-03-05 14:53:02 -0600

    Add poke-soa-gateaway as regular folder with all files

---
+++ poke-soa-gateaway/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { config } from './config/env.js';
+import { corsMiddleware } from './middlewares/cors.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import gatewayRoutes from './routes/gatewayRoutes.js';
+
+const app = express();
+
+//  Middlewares globales
+app.use(express.json());
+app.use(corsMiddleware);
+
+
+app.use(gatewayRoutes);
+
+// Manejo de errores 
+app.use(errorHandler);
+
+//  Inicializar servidor
+app.listen(config.port, () => {
+  console.log(`API Gateway enrutando de forma segura en http://localhost:${config.port}`);
+});
